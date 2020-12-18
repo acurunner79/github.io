2321b0d1d888bab03368715968b48ccb9e2efd5d
Jorge Soto <djsmoothzouk@gmail.com>
2020-12-18 16:44:23 -0600

    ajax added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-console.log("hello world");
+
+/////////////////////////////////////////
+// PULL AND RENDER DATA FROM GOOGLE SHEET
+/////////////////////////////////////////
+
+
+$.ajax("https://spreadsheets.google.com/feeds/list/1GngbnJmQbGTRqSRAIn0GDWu3cRhh_9tudbz8fXd1u2Q/1/public/full?alt=json")
+.then((data) => {
+    // Checking my data
+    console.log(data)
+
+    // Put our projects in a cariable
+    const rawProjects = data.feed.entry
+
+    console.log(rawProjects)
+
+    // Prettify our projects array
+    const projects = rawProjects.map((project) => {
+        return{
+            name: project.gsx$name.$t,
+            img: project.gsx$img.$t,
+            description: project.gsx$description.$t,
+            live: project.gsx$live.$t,
+            github: project.gsx$github.$t
+
+        }
+    })
+
+    console.log(projects)
+
+/////////////////////////////////////////
+//  USE JQUERY TO RENDER PROJECTS TO PAGE
+//////////////////////////////////////////
+
+
+})
+
+
+
+
+
+
+// PULL DATA
