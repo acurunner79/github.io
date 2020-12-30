429c7b5108dd8d8cbbd18b43f33e0f32aa844649
Jorge Soto <djsmoothzouk@gmail.com>
2020-12-30 13:19:14 -0600

    my project cards inserted

--- js/app.js
+++ js/app.js
@@ -41,3 +41,17 @@ $.ajax("https://spreadsheets.google.com/feeds/list/1GngbnJmQbGTRqSRAIn0GDWu3cRhh
 
 
 // PULL DATA
+
+// for (i=0;i < projects.length; i++) {
+//     const $div = $(
+//         `<div class="card" style="width: 18rem;">
+//         <img src=${projects[i].img} class="card-img-top" alt="project">
+//         <div class="card-body">
+//         <h5 class="card-title">${projects[i].name}</h5>
+//         <p class="card-text">${projects[i].description}</p>
+//         <a href=${projects[i].live} class="btn">Check it out</a>
+//         </div>
+//         </div>`
+//     )
+// $("#projects").append($div)
+// }
